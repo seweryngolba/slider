d7e2445845821b72c7d5d342ccb2391c481a80d3
Seweryn Golba <sgoolb@gmail.com>
2023-05-03 19:02:39 +0200

    final commit

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ let activeSlideNumber = 1;
 let arrowLeft = document.querySelector(".fa-caret-left");
 let arrowRight = document.querySelector(".fa-caret-right");
 
-let dot1 = document.querySelector("#dot1");
-let dot2 = document.querySelector("#dot2");
-let dot3 = document.querySelector("#dot3");
-
-let slide1 = document.querySelector("#slide1");
-let slide2 = document.querySelector("#slide2");
-let slide3 = document.querySelector("#slide3");
-
 let hideActiveSlide = () => {
   let activeElement = document.querySelector(".active");
   activeElement.classList.remove("active");
@@ -38,21 +30,13 @@ let showPreviousSlide = () => {
   showSlide(activeSlideNumber);
 };
 
-let showSlide1 = () => {
-  showSlide(1);
-};
-
-let showSlide2 = () => {
-  showSlide(2);
-};
-
-let showSlide3 = () => {
-  showSlide(3);
-};
-
-dot1.addEventListener("click", showSlide1);
-dot2.addEventListener("click", showSlide2);
-dot3.addEventListener("click", showSlide3);
+for (let i = 1; i <= 3; i++) {
+  let showSlideI = () => {
+    activeSlideNumber = i;
+    showSlide(i);
+  };
+  document.querySelector("#dot" + i).addEventListener("click", showSlideI);
+}
 
 arrowRight.addEventListener("click", showNextSlide);
 arrowLeft.addEventListener("click", showPreviousSlide);
